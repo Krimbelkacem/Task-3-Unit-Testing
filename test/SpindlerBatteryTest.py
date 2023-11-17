7607a8158b5111ae6ab583a5705e428a7835c6b1
unknown <krimbelkacem.hamnache@fgei.ummto.dz>
2023-11-17 11:54:18 +0100

    test

--- test/SpindlerBatteryTest.py
+++ test/SpindlerBatteryTest.py
@@ -6,13 +6,13 @@ class TestSpindlerBattery(unittest.TestCase):
     def test_needs_service_when_required(self):
         # Test if battery needs service when required
         battery = SpindlerBattery(
-            last_service_date="2023-01-01", current_date="2023-11-15")
+            last_service_date="2023-01-01", current_date="2023-11-17")
         self.assertTrue(battery.needs_service())
 
     def test_does_not_need_service_when_not_required(self):
         # Test if battery doesn't need service when it's not required
         battery = SpindlerBattery(
-            last_service_date="2023-11-01", current_date="2023-11-15")
+            last_service_date="2023-11-01", current_date="2023-11-17")
         self.assertFalse(battery.needs_service())
 
 
